b689f4bbab55818673d0470679e173181be90a36
dtao <daniel.tao@gmail.com>
2011-11-28 14:34:50 -0800

    implemented floating div-based unicode output (don't like it, will try something else)

--- unicodey.js
+++ unicodey.js
@@ -1,16 +1,45 @@
 $(document).ready(function() {
   var $input = $('#input'),
-      $output = $('#output');
+      $output = $('#output'),
+      iterator;
+  
+  function eachCharAsync(string, func) {
+    var upper = string.length - 1,
+        timeoutId;
+    
+    (function iterate(index) {
+      timeoutId = setTimeout(function() {
+        func(string.charAt(index), string.charCodeAt(index));
+        if (index < upper) {
+          iterate(index + 1);
+        } else {
+          timeoutId = null;
+        }
+      }, 1);
+    }(0));
+
+    return {
+      stop: function() {
+        if (timeoutId) {
+          clearTimeout(timeoutId);
+        }
+      }
+    };
+  }
+
+  function displayUnicode(char, charCode) {
+    var $block = $('<div>').addClass('block'),
+        $charBox = $('<div>').addClass('char-box').text(char).appendTo($block),
+        $codeBox = $('<div>').addClass('code-box').text(charCode).appendTo($block);
+    
+    $block.appendTo($output);
+  }
   
   $input.keyup(function() {
-    var text = $input.val(),
-        output = [],
-        i;
-    
-    for (i = 0; i < text.length; i++) {
-      output.push(text.charCodeAt(i));
+    if (iterator) {
+      iterator.stop();
     }
-
-    $output.text(output.join(' '));
+    $output.empty();
+    iterator = eachCharAsync($input.val(), displayUnicode);
   });
 });
